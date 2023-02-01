ccedc4ce1b7bae9f5bfc0732f8bf87d62fdcdd3d
vellimole0621 <vellimole0621@gmail.com>
2023-02-01 23:05:35 +0900

    today's plus learning

--- RubyFile/first.rb
+++ RubyFile/first.rb
@@ -1,12 +1,5 @@
-colors = ["red","blue"]
+def sayHi(name)
+puts "hi! #{name}"
+end
 
-colors[0] = "pink" #직접 인덱스 연결
-colors[1..2] = ["white", "black"] # 범위 연결
-colors.push("gold") # 끝에 연결
-colors << "silver" # 끝에 연결2
-
-p colors
-
-colors.sort!
-
-p colors
+sayHi("taguchi")
