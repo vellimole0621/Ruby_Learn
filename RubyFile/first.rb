acfb9c999e21f8ec9f61a1604e25c1b28db7e5e4
vellimole0621 <vellimole0621@gmail.com>
2023-02-02 12:27:29 +0900

    230202

--- RubyFile/first.rb
+++ RubyFile/first.rb
@@ -1,5 +1,23 @@
-def sayHi(name)
-puts "hi! #{name}"
+
+class User
+
+  @@count = 0
+
+    VERSION = 1.1
+
+  def initialize(name)
+    @@count += 1
+    @name = name
+  end
+
+  def self.info
+    puts "#{VERSION}: User Class, #{@@count} instances."
+  end
+
 end
 
-sayHi("taguchi")
+tom = User.new("tom")
+bob = User.new("bob")
+steve = User.new("steve")
+User.info
+p User::VERSION 
